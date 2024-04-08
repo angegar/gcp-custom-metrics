5585dbabe3dfeceeab3d8253ee0f4d48871d35b0
Laurent Gil <lg7983@gmail.com>
2024-04-08 20:00:30 +0000

    chore: refactor with object model
    Signed-off-by: Laurent Gil <lg7983@gmail.com>

--- metric-descriptor.py
+++ metric-descriptor.py
@@ -3,65 +3,78 @@ from google.api import metric_pb2 as ga_metric
 from google.cloud import monitoring_v3
 import time, random
 
-# CREATE DESCRIPTOR
-project_id="bds-rd-sf3-dev"
-location="europe-west1"
-client = monitoring_v3.MetricServiceClient()
-project_name = f"projects/{project_id}"
-descriptor = ga_metric.MetricDescriptor()
-descriptor.type = "custom.googleapis.com/lab/vpn/bandwidth/download"
-descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.GAUGE
-descriptor.value_type = ga_metric.MetricDescriptor.ValueType.DOUBLE
-descriptor.description = "Download speed from the lab in byte per second"
+# Authenticate with gcloud auth application-default login
 
-labels = ga_label.LabelDescriptor()
-labels.key = "from"
-labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
-labels.description = "Source test server"
-descriptor.labels.append(labels)
+class TimeSerie:
+    def __init__(self):
+        self.client = monitoring_v3.MetricServiceClient()
+        self.projectId = "bds-rd-sf3-dev"
+        self.location = "europe-west1"
+        self.projectName = f"projects/{self.projectId}"
+        self.type = "custom.googleapis.com/lab/vpn/bandwidth/download2"
+       
+    # def createDescriptor(self):
+    #     # CREATE DESCRIPTOR
+    #     # project_name = f"projects/{project_id}"
+    #     descriptor = ga_metric.MetricDescriptor()
+    #     descriptor.type = "custom.googleapis.com/lab/vpn/bandwidth/download"
+    #     descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.GAUGE
+    #     descriptor.value_type = ga_metric.MetricDescriptor.ValueType.DOUBLE
+    #     descriptor.description = "Download speed from the lab in byte per second"
 
-labels = ga_label.LabelDescriptor()
-labels.key = "to"
-labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
-labels.description = "Destination test server"
-descriptor.labels.append(labels)
+    #     labels = ga_label.LabelDescriptor()
+    #     labels.key = "from"
+    #     labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
+    #     labels.description = "Source test server"
+    #     descriptor.labels.append(labels)
 
-descriptor = client.create_metric_descriptor(
-    name=project_name, metric_descriptor=descriptor
-)
-print("Created {}.".format(descriptor.name))
+    #     labels = ga_label.LabelDescriptor()
+    #     labels.key = "to"
+    #     labels.value_type = ga_label.LabelDescriptor.ValueType.STRING
+    #     labels.description = "Destination test server"
+    #     descriptor.labels.append(labels)
+
+    #     descriptor = self.client.create_metric_descriptor(
+    #         name=self.projectName, metric_descriptor=descriptor
+    #     )
+    #     print("Created {}.".format(descriptor.name))
+
+    def writeTimeSerie(self, speed=random.uniform(10.5, 75.5), sourceServer="sf-centrale", destinationServer="artifactory"):
+        series = monitoring_v3.TimeSeries()
+        series.metric.type = self.type
+        series.metric.labels["from"] = sourceServer
+        series.metric.labels["to"] = destinationServer
+
+        series.resource.type = "generic_task"
+        series.resource.labels["namespace"] = "ech-monitoring"
+        series.resource.labels["project_id"] = self.projectId
+        series.resource.labels["location"] = self.location
+        series.resource.labels["job"] = "lab-vpn"
+        series.resource.labels["task_id"] = "lab-vpn"
+
+        now = time.time()
+        seconds = int(now)
+        nanos = int((now - seconds) * 10**9)
+        interval = monitoring_v3.TimeInterval(
+            {"end_time": {"seconds": seconds, "nanos": nanos}}
+        )
+
+        point = monitoring_v3.Point({"interval": interval, "value": {"double_value": speed}})
+        series.points = [point]
+
+        self.client.create_time_series(name=self.projectName, time_series=[series])
+        print("Done writing time series.")
 
 
-# WRITE METRICS
-# from google.cloud import monitoring_v3
+    def deleteTimeSerie(self):
+        descriptor_name :str = "projects/bds-rd-sf3-dev/metricDescriptors/custom.googleapis.com/lab/vpn/bandwidth/download2"
+        self.client.delete_metric_descriptor(name=descriptor_name)
+        print("Deleted metric descriptor {}.".format(descriptor_name))
 
-# client = monitoring_v3.MetricServiceClient()
-# project_name = f"projects/{project_id}"
-
-series = monitoring_v3.TimeSeries()
-
-series.metric.type = descriptor.type
-series.metric.labels["from"] = "sf-centrale"
-series.metric.labels["to"] = "artifactory"
-
-series.resource.type = "generic_task"
-series.resource.labels["namespace"] = "ech-monitoring"
-series.resource.labels["project_id"] = project_id
-series.resource.labels["location"] = location
-series.resource.labels["job"] = "lab-vpn"
-series.resource.labels["task_id"] = "lab-vpn"
-
-now = time.time()
-seconds = int(now)
-nanos = int((now - seconds) * 10**9)
-interval = monitoring_v3.TimeInterval(
-    {"end_time": {"seconds": seconds, "nanos": nanos}}
-)
-
-speed=random.uniform(10.5, 75.5)
-
-point = monitoring_v3.Point({"interval": interval, "value": {"double_value": speed}})
-# point = monitoring_v3.Point({"value": 68.5})
-series.points = [point]
-
-client.create_time_series(name=project_name, time_series=[series])
+data = TimeSerie()
+# data.deleteTimeSerie()
+for i in range(50):
+    data.writeTimeSerie()
+    time.sleep(10)
+    print(f"Data {i} written")
+    i += 1
